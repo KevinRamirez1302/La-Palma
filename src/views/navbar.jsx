1731b05d9514d655ecc6d6a2fcc39970e458f018
Kevin <kevin.alexanderra1302@gmail.com>
2024-09-30 07:19:11 -0400

    pc de mesa

--- src/views/navbar.jsx
+++ src/views/navbar.jsx
@@ -1,20 +1,18 @@
 export const Navbar = () => {
   return (
-    <>
-      <nav className=" flex justify-between absolute">
-        <p>Navbar</p>
-        <ul className=" flextext-white font-bold">
-          <li>
-            <a href="">Enjoy</a>
-          </li>
-          <li>
-            <a href="">History</a>
-          </li>
-          <li>
-            <a href="">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
-};
+    <nav className=" pt-3 absolute flex w-full justify-around">
+      <p className=" font-bold text-[25px]">Hola</p>
+      <ul className="flex text-[22px] gap-4 text-white font-bold">
+        <li>
+          <a href="">Enjoy</a>
+        </li>
+        <li>
+          <a href="">History</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
